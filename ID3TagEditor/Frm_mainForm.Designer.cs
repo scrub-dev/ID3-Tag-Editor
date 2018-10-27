86d2a8c0c6b8fb1558d258662f3dd9da5b9dccdd
Scott Donaldson <scott-donaldson@outlook.com>
2018-10-28 00:40:55 +0100

    Main Feature update
    Added:
    Write
    Erase
    Erase All
    Main Error Handling
    
    TODO: 
    More Error Handling
    Finish with Buttons
    Add Settings
    Add Help Section to Github and Main Form

--- ID3TagEditor/Frm_mainForm.Designer.cs
+++ ID3TagEditor/Frm_mainForm.Designer.cs
@@ -32,19 +32,44 @@
             this.Btn_selectFolder = new System.Windows.Forms.Button();
             this.Rtb_output = new System.Windows.Forms.RichTextBox();
             this.Lbx_files = new System.Windows.Forms.ListBox();
+            this.Lbl_selectedSong = new System.Windows.Forms.Label();
+            this.Txt_selectedSong = new System.Windows.Forms.TextBox();
+            this.Txt_artist = new System.Windows.Forms.TextBox();
+            this.Txt_song = new System.Windows.Forms.TextBox();
+            this.Txt_album = new System.Windows.Forms.TextBox();
+            this.Txt_year = new System.Windows.Forms.TextBox();
+            this.Lbl_artist = new System.Windows.Forms.Label();
+            this.Lbl_song = new System.Windows.Forms.Label();
+            this.Lbl_album = new System.Windows.Forms.Label();
+            this.Lbl_year = new System.Windows.Forms.Label();
+            this.Chk_artist = new System.Windows.Forms.CheckBox();
+            this.Chk_song = new System.Windows.Forms.CheckBox();
+            this.Chk_album = new System.Windows.Forms.CheckBox();
+            this.Chk_year = new System.Windows.Forms.CheckBox();
+            this.Btn_previous = new System.Windows.Forms.Button();
+            this.Btn_write = new System.Windows.Forms.Button();
+            this.Btn_next = new System.Windows.Forms.Button();
+            this.Ms_mainMenuStrip = new System.Windows.Forms.MenuStrip();
+            this.settingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.enableToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutUsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.reportABugToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.Btn_eraseAll = new System.Windows.Forms.Button();
+            this.Btn_erase = new System.Windows.Forms.Button();
+            this.Ms_mainMenuStrip.SuspendLayout();
             this.SuspendLayout();
             // 
             // Txt_folderSelected
             // 
-            this.Txt_folderSelected.Location = new System.Drawing.Point(100, 18);
+            this.Txt_folderSelected.Location = new System.Drawing.Point(100, 27);
             this.Txt_folderSelected.Name = "Txt_folderSelected";
             this.Txt_folderSelected.Size = new System.Drawing.Size(191, 20);
             this.Txt_folderSelected.TabIndex = 0;
-            this.Txt_folderSelected.TextChanged += new System.EventHandler(this.Txt_folderSelected_TextChanged);
             // 
             // Btn_selectFolder
             // 
-            this.Btn_selectFolder.Location = new System.Drawing.Point(12, 16);
+            this.Btn_selectFolder.Location = new System.Drawing.Point(12, 25);
             this.Btn_selectFolder.Name = "Btn_selectFolder";
             this.Btn_selectFolder.Size = new System.Drawing.Size(82, 23);
             this.Btn_selectFolder.TabIndex = 1;
@@ -54,35 +79,282 @@
             // 
             // Rtb_output
             // 
-            this.Rtb_output.Location = new System.Drawing.Point(12, 342);
+            this.Rtb_output.Location = new System.Drawing.Point(12, 256);
             this.Rtb_output.Name = "Rtb_output";
             this.Rtb_output.ReadOnly = true;
-            this.Rtb_output.Size = new System.Drawing.Size(279, 96);
+            this.Rtb_output.Size = new System.Drawing.Size(279, 178);
             this.Rtb_output.TabIndex = 2;
             this.Rtb_output.Text = "";
+            this.Rtb_output.TextChanged += new System.EventHandler(this.Rtb_output_TextChanged);
             // 
             // Lbx_files
             // 
             this.Lbx_files.FormattingEnabled = true;
-            this.Lbx_files.Location = new System.Drawing.Point(297, 18);
+            this.Lbx_files.Location = new System.Drawing.Point(304, 25);
             this.Lbx_files.Name = "Lbx_files";
-            this.Lbx_files.Size = new System.Drawing.Size(282, 420);
+            this.Lbx_files.Size = new System.Drawing.Size(292, 407);
             this.Lbx_files.TabIndex = 3;
+            this.Lbx_files.SelectedIndexChanged += new System.EventHandler(this.Lbx_files_SelectedIndexChanged);
+            // 
+            // Lbl_selectedSong
+            // 
+            this.Lbl_selectedSong.AutoSize = true;
+            this.Lbl_selectedSong.Location = new System.Drawing.Point(14, 55);
+            this.Lbl_selectedSong.Name = "Lbl_selectedSong";
+            this.Lbl_selectedSong.Size = new System.Drawing.Size(80, 13);
+            this.Lbl_selectedSong.TabIndex = 7;
+            this.Lbl_selectedSong.Text = "Selected Song:";
+            // 
+            // Txt_selectedSong
+            // 
+            this.Txt_selectedSong.Enabled = false;
+            this.Txt_selectedSong.Location = new System.Drawing.Point(100, 52);
+            this.Txt_selectedSong.Name = "Txt_selectedSong";
+            this.Txt_selectedSong.Size = new System.Drawing.Size(191, 20);
+            this.Txt_selectedSong.TabIndex = 8;
+            // 
+            // Txt_artist
+            // 
+            this.Txt_artist.Enabled = false;
+            this.Txt_artist.Location = new System.Drawing.Point(100, 79);
+            this.Txt_artist.Name = "Txt_artist";
+            this.Txt_artist.Size = new System.Drawing.Size(100, 20);
+            this.Txt_artist.TabIndex = 9;
+            // 
+            // Txt_song
+            // 
+            this.Txt_song.Enabled = false;
+            this.Txt_song.Location = new System.Drawing.Point(100, 105);
+            this.Txt_song.Name = "Txt_song";
+            this.Txt_song.Size = new System.Drawing.Size(100, 20);
+            this.Txt_song.TabIndex = 10;
+            // 
+            // Txt_album
+            // 
+            this.Txt_album.Enabled = false;
+            this.Txt_album.Location = new System.Drawing.Point(100, 131);
+            this.Txt_album.Name = "Txt_album";
+            this.Txt_album.Size = new System.Drawing.Size(100, 20);
+            this.Txt_album.TabIndex = 11;
+            // 
+            // Txt_year
+            // 
+            this.Txt_year.Enabled = false;
+            this.Txt_year.Location = new System.Drawing.Point(100, 157);
+            this.Txt_year.Name = "Txt_year";
+            this.Txt_year.Size = new System.Drawing.Size(100, 20);
+            this.Txt_year.TabIndex = 12;
+            // 
+            // Lbl_artist
+            // 
+            this.Lbl_artist.AutoSize = true;
+            this.Lbl_artist.Location = new System.Drawing.Point(32, 82);
+            this.Lbl_artist.Name = "Lbl_artist";
+            this.Lbl_artist.Size = new System.Drawing.Size(62, 13);
+            this.Lbl_artist.TabIndex = 13;
+            this.Lbl_artist.Text = "Artist name:";
+            // 
+            // Lbl_song
+            // 
+            this.Lbl_song.AutoSize = true;
+            this.Lbl_song.Location = new System.Drawing.Point(30, 108);
+            this.Lbl_song.Name = "Lbl_song";
+            this.Lbl_song.Size = new System.Drawing.Size(64, 13);
+            this.Lbl_song.TabIndex = 14;
+            this.Lbl_song.Text = "Song name:";
+            // 
+            // Lbl_album
+            // 
+            this.Lbl_album.AutoSize = true;
+            this.Lbl_album.Location = new System.Drawing.Point(26, 134);
+            this.Lbl_album.Name = "Lbl_album";
+            this.Lbl_album.Size = new System.Drawing.Size(68, 13);
+            this.Lbl_album.TabIndex = 15;
+            this.Lbl_album.Text = "Album name:";
+            // 
+            // Lbl_year
+            // 
+            this.Lbl_year.AutoSize = true;
+            this.Lbl_year.Location = new System.Drawing.Point(33, 160);
+            this.Lbl_year.Name = "Lbl_year";
+            this.Lbl_year.Size = new System.Drawing.Size(61, 13);
+            this.Lbl_year.TabIndex = 16;
+            this.Lbl_year.Text = "Year made:";
+            // 
+            // Chk_artist
+            // 
+            this.Chk_artist.AutoSize = true;
+            this.Chk_artist.Location = new System.Drawing.Point(207, 81);
+            this.Chk_artist.Name = "Chk_artist";
+            this.Chk_artist.Size = new System.Drawing.Size(85, 17);
+            this.Chk_artist.TabIndex = 17;
+            this.Chk_artist.Text = "Enable Artist";
+            this.Chk_artist.UseVisualStyleBackColor = true;
+            this.Chk_artist.CheckedChanged += new System.EventHandler(this.Chk_artist_CheckedChanged);
+            // 
+            // Chk_song
+            // 
+            this.Chk_song.AutoSize = true;
+            this.Chk_song.Location = new System.Drawing.Point(207, 107);
+            this.Chk_song.Name = "Chk_song";
+            this.Chk_song.Size = new System.Drawing.Size(87, 17);
+            this.Chk_song.TabIndex = 18;
+            this.Chk_song.Text = "Enable Song";
+            this.Chk_song.UseVisualStyleBackColor = true;
+            this.Chk_song.CheckedChanged += new System.EventHandler(this.Chk_song_CheckedChanged);
+            // 
+            // Chk_album
+            // 
+            this.Chk_album.AutoSize = true;
+            this.Chk_album.Location = new System.Drawing.Point(207, 133);
+            this.Chk_album.Name = "Chk_album";
+            this.Chk_album.Size = new System.Drawing.Size(91, 17);
+            this.Chk_album.TabIndex = 19;
+            this.Chk_album.Text = "Enable Album";
+            this.Chk_album.UseVisualStyleBackColor = true;
+            this.Chk_album.CheckedChanged += new System.EventHandler(this.Chk_album_CheckedChanged);
+            // 
+            // Chk_year
+            // 
+            this.Chk_year.AutoSize = true;
+            this.Chk_year.Cursor = System.Windows.Forms.Cursors.Default;
+            this.Chk_year.Location = new System.Drawing.Point(207, 159);
+            this.Chk_year.Name = "Chk_year";
+            this.Chk_year.Size = new System.Drawing.Size(84, 17);
+            this.Chk_year.TabIndex = 20;
+            this.Chk_year.Text = "Enable Year";
+            this.Chk_year.UseVisualStyleBackColor = true;
+            this.Chk_year.CheckedChanged += new System.EventHandler(this.Chk_year_CheckedChanged);
+            // 
+            // Btn_previous
+            // 
+            this.Btn_previous.Location = new System.Drawing.Point(19, 199);
+            this.Btn_previous.Name = "Btn_previous";
+            this.Btn_previous.Size = new System.Drawing.Size(90, 23);
+            this.Btn_previous.TabIndex = 21;
+            this.Btn_previous.Text = "Previous";
+            this.Btn_previous.UseVisualStyleBackColor = true;
+            // 
+            // Btn_write
+            // 
+            this.Btn_write.Location = new System.Drawing.Point(115, 199);
+            this.Btn_write.Name = "Btn_write";
+            this.Btn_write.Size = new System.Drawing.Size(90, 23);
+            this.Btn_write.TabIndex = 22;
+            this.Btn_write.Text = "Write";
+            this.Btn_write.UseVisualStyleBackColor = true;
+            this.Btn_write.Click += new System.EventHandler(this.Btn_write_Click);
+            // 
+            // Btn_next
+            // 
+            this.Btn_next.Location = new System.Drawing.Point(212, 199);
+            this.Btn_next.Name = "Btn_next";
+            this.Btn_next.Size = new System.Drawing.Size(86, 23);
+            this.Btn_next.TabIndex = 23;
+            this.Btn_next.Text = "Next";
+            this.Btn_next.UseVisualStyleBackColor = true;
+            // 
+            // Ms_mainMenuStrip
+            // 
+            this.Ms_mainMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.settingsToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.Ms_mainMenuStrip.Location = new System.Drawing.Point(0, 0);
+            this.Ms_mainMenuStrip.Name = "Ms_mainMenuStrip";
+            this.Ms_mainMenuStrip.Size = new System.Drawing.Size(596, 24);
+            this.Ms_mainMenuStrip.TabIndex = 24;
+            this.Ms_mainMenuStrip.Text = "menuStrip1";
+            // 
+            // settingsToolStripMenuItem
+            // 
+            this.settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
+            this.settingsToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.settingsToolStripMenuItem.Text = "Settings";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.enableToolStripMenuItem,
+            this.aboutUsToolStripMenuItem,
+            this.reportABugToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // enableToolStripMenuItem
+            // 
+            this.enableToolStripMenuItem.Name = "enableToolStripMenuItem";
+            this.enableToolStripMenuItem.Size = new System.Drawing.Size(154, 22);
+            this.enableToolStripMenuItem.Text = "Enable Tooltips";
+            // 
+            // aboutUsToolStripMenuItem
+            // 
+            this.aboutUsToolStripMenuItem.Name = "aboutUsToolStripMenuItem";
+            this.aboutUsToolStripMenuItem.Size = new System.Drawing.Size(154, 22);
+            this.aboutUsToolStripMenuItem.Text = "About Us";
+            // 
+            // reportABugToolStripMenuItem
+            // 
+            this.reportABugToolStripMenuItem.Name = "reportABugToolStripMenuItem";
+            this.reportABugToolStripMenuItem.Size = new System.Drawing.Size(154, 22);
+            this.reportABugToolStripMenuItem.Text = "Report a Bug";
+            // 
+            // Btn_eraseAll
+            // 
+            this.Btn_eraseAll.Location = new System.Drawing.Point(65, 227);
+            this.Btn_eraseAll.Name = "Btn_eraseAll";
+            this.Btn_eraseAll.Size = new System.Drawing.Size(90, 23);
+            this.Btn_eraseAll.TabIndex = 25;
+            this.Btn_eraseAll.Text = "Erase All";
+            this.Btn_eraseAll.UseVisualStyleBackColor = true;
+            this.Btn_eraseAll.Click += new System.EventHandler(this.Btn_eraseAll_Click);
+            // 
+            // Btn_erase
+            // 
+            this.Btn_erase.Location = new System.Drawing.Point(161, 227);
+            this.Btn_erase.Name = "Btn_erase";
+            this.Btn_erase.Size = new System.Drawing.Size(90, 23);
+            this.Btn_erase.TabIndex = 26;
+            this.Btn_erase.Text = "Erase Tag";
+            this.Btn_erase.UseVisualStyleBackColor = true;
+            this.Btn_erase.Click += new System.EventHandler(this.Btn_erase_Click);
             // 
             // Frm_mainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(591, 450);
+            this.ClientSize = new System.Drawing.Size(596, 450);
+            this.Controls.Add(this.Btn_erase);
+            this.Controls.Add(this.Btn_eraseAll);
+            this.Controls.Add(this.Btn_next);
+            this.Controls.Add(this.Btn_write);
+            this.Controls.Add(this.Btn_previous);
+            this.Controls.Add(this.Chk_year);
+            this.Controls.Add(this.Chk_album);
+            this.Controls.Add(this.Chk_song);
+            this.Controls.Add(this.Chk_artist);
+            this.Controls.Add(this.Lbl_year);
+            this.Controls.Add(this.Lbl_album);
+            this.Controls.Add(this.Lbl_song);
+            this.Controls.Add(this.Lbl_artist);
+            this.Controls.Add(this.Txt_year);
+            this.Controls.Add(this.Txt_album);
+            this.Controls.Add(this.Txt_song);
+            this.Controls.Add(this.Txt_artist);
+            this.Controls.Add(this.Txt_selectedSong);
+            this.Controls.Add(this.Lbl_selectedSong);
             this.Controls.Add(this.Lbx_files);
             this.Controls.Add(this.Rtb_output);
             this.Controls.Add(this.Btn_selectFolder);
             this.Controls.Add(this.Txt_folderSelected);
+            this.Controls.Add(this.Ms_mainMenuStrip);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "Frm_mainForm";
             this.ShowIcon = false;
             this.Text = "ID3 Tag Editor";
             this.Load += new System.EventHandler(this.Frm_mainForm_Load);
+            this.Ms_mainMenuStrip.ResumeLayout(false);
+            this.Ms_mainMenuStrip.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -94,6 +366,31 @@
         private System.Windows.Forms.Button Btn_selectFolder;
         private System.Windows.Forms.RichTextBox Rtb_output;
         private System.Windows.Forms.ListBox Lbx_files;
+        private System.Windows.Forms.Label Lbl_selectedSong;
+        private System.Windows.Forms.TextBox Txt_selectedSong;
+        private System.Windows.Forms.TextBox Txt_artist;
+        private System.Windows.Forms.TextBox Txt_song;
+        private System.Windows.Forms.TextBox Txt_album;
+        private System.Windows.Forms.TextBox Txt_year;
+        private System.Windows.Forms.Label Lbl_artist;
+        private System.Windows.Forms.Label Lbl_song;
+        private System.Windows.Forms.Label Lbl_album;
+        private System.Windows.Forms.Label Lbl_year;
+        private System.Windows.Forms.CheckBox Chk_artist;
+        private System.Windows.Forms.CheckBox Chk_song;
+        private System.Windows.Forms.CheckBox Chk_album;
+        private System.Windows.Forms.CheckBox Chk_year;
+        private System.Windows.Forms.Button Btn_previous;
+        private System.Windows.Forms.Button Btn_write;
+        private System.Windows.Forms.Button Btn_next;
+        private System.Windows.Forms.MenuStrip Ms_mainMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem settingsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem enableToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutUsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem reportABugToolStripMenuItem;
+        private System.Windows.Forms.Button Btn_eraseAll;
+        private System.Windows.Forms.Button Btn_erase;
     }
 }
 
--- ID3TagEditor/Frm_mainForm.cs
+++ ID3TagEditor/Frm_mainForm.cs
@@ -8,27 +8,52 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
+using TagLib;
 
 namespace ID3TagEditor
 {
     public partial class Frm_mainForm : Form
     {
-        public String directory = "";
-        //public string[] Files;
-        public int selected = 0;
+        public String directory = ""; 
+        public string[] Files; 
+        public int selected = 0; 
 
         public Frm_mainForm()
         {
-            InitializeComponent();
+            InitializeComponent(); 
         }
 
-        public void ChooseFolder()
+        public void ChooseFolder() 
         {
-            FolderBrowserDialog Fbd_selectDirectory = new FolderBrowserDialog();
-            if(Fbd_selectDirectory.ShowDialog() == DialogResult.OK)
+            FolderBrowserDialog Fbd_selectDirectory = new FolderBrowserDialog(); 
+            if(Fbd_selectDirectory.ShowDialog() == DialogResult.OK) 
             {
                 Txt_folderSelected.Text = Fbd_selectDirectory.SelectedPath;
                 directory = Fbd_selectDirectory.SelectedPath;
+
+                Files = Directory.GetFiles(Txt_folderSelected.Text, "*.mp3");
+                Rtb_output.AppendText("\n" + Files.Length + " MP3 File(s) found");
+                if (Files.Length == 0)
+                {
+                    DialogResult dr = MessageBox.Show("You selected a folder with no MP3 Files in it, Would you like to pick another folder?", "No Files Found", MessageBoxButtons.YesNo, MessageBoxIcon.Error);
+                    switch (dr)
+                    {
+                        case DialogResult.Yes:
+                            ChooseFolder();
+                            break;
+                        case DialogResult.No:
+                            return;
+                    }
+                }
+                else
+                {
+                    for (int i = 0; i < Files.Length; i++)
+                    {
+                        Lbx_files.Items.Add(Files[i].Substring(Txt_folderSelected.TextLength + 1));
+                    }
+                    Lbx_files.SetSelected(selected, true);
+                }
+
             }
         }
 
@@ -39,19 +64,130 @@ namespace ID3TagEditor
 
         private void Btn_selectFolder_Click(object sender, EventArgs e)
         {
+            if (Lbx_files.Items != null) Lbx_files.Items.Clear();
             ChooseFolder();
         }
 
-        private void Txt_folderSelected_TextChanged(object sender, EventArgs e)
+        private void Rtb_output_TextChanged(object sender, EventArgs e)
         {
-            string[] Files = Directory.GetFiles(@Txt_folderSelected.Text, "*.mp3");
-            Rtb_output.AppendText("\n"+Files.Length +" MP3 File(s) found");
+            Rtb_output.ScrollToCaret();
+        }
 
-            for(int i = 0; i < Files.Length; i++)
+        private void Lbx_files_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Txt_selectedSong.Text = Lbx_files.SelectedItem.ToString();
+        }
+
+        private void Chk_artist_CheckedChanged(object sender, EventArgs e)
+        {
+            if (Chk_artist.Checked)
             {
-                Lbx_files.Items.Add(Files[i].Substring(Txt_folderSelected.TextLength + 1));
+                Txt_artist.Enabled = true;
             }
-            Lbx_files.SetSelected(selected, true);
+            else
+            {
+                Txt_artist.Enabled = false;
+                Txt_artist.Text = "";
+            }
+        }
+
+        private void Chk_song_CheckedChanged(object sender, EventArgs e)
+        {
+            if (Chk_song.Checked)
+            {
+                Txt_song.Enabled = true;
+            }
+            else
+            {
+                Txt_song.Enabled = false;
+                Txt_song.Text = "";
+            }
+        }
+
+        private void Chk_album_CheckedChanged(object sender, EventArgs e)
+        {
+            if (Chk_album.Checked)
+            {
+                Txt_album.Enabled = true;
+            }
+            else
+            {
+                Txt_album.Enabled = false;
+                Txt_album.Text = "";
+            }
+        }
+
+        private void Chk_year_CheckedChanged(object sender, EventArgs e)
+        {
+            if (Chk_year.Checked)
+            {
+                Txt_year.Enabled = true;
+            }
+            else
+            {
+                Txt_year.Enabled = false;
+                Txt_year.Text = "";
+            }
+        }
+
+        private void Btn_write_Click(object sender, EventArgs e)
+        {
+            if (Chk_song.Checked && Txt_song.Text == "") { MessageBox.Show("Song field enabled but left blank", "Song Field Blank", MessageBoxButtons.OK, MessageBoxIcon.Error); return; }
+            if (Chk_artist.Checked && Txt_artist.Text == "") { MessageBox.Show("Artist field enabled but left blank", "Artist Field Blank", MessageBoxButtons.OK, MessageBoxIcon.Error); return; }
+            if (Chk_album.Checked && Txt_album.Text == "") { MessageBox.Show("Album field enabled but left blank", "Album Field Blank", MessageBoxButtons.OK, MessageBoxIcon.Error); return; }
+            if (Chk_year.Checked && Txt_year.Text == "") { MessageBox.Show("Year field enabled but left blank", "Year Field Blank", MessageBoxButtons.OK, MessageBoxIcon.Error); return; }
+            try
+            {
+                TagLib.File MetaFile = TagLib.File.Create(Txt_folderSelected.Text + "\\" + Txt_selectedSong.Text);
+
+                if (Chk_song.Checked) MetaFile.Tag.Title = Txt_song.Text;
+                if (Chk_artist.Checked) MetaFile.Tag.Performers = new string[1] { Txt_artist.Text };
+                if (Chk_album.Checked) MetaFile.Tag.Album = Txt_album.Text;
+                if (Chk_year.Checked) MetaFile.Tag.Year = Convert.ToUInt16(Txt_year.Text);
+                MetaFile.Save();
+                Rtb_output.AppendText("\n" + Txt_selectedSong.Text + " : Tags have been wrote");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message + "\n" + Txt_folderSelected.Text + "\\" + Txt_selectedSong.Text, "An Error Occured", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Console.WriteLine(ex);
+            }
+        }
+
+        private void Btn_eraseAll_Click(object sender, EventArgs e)
+        {
+            DialogResult dr = MessageBox.Show("You are about to wipe all Meta Tags from all MP3 Files in the selected Directory", "Are you sure?", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning);
+            switch (dr)
+            {
+                case DialogResult.OK:
+                    for (int i = 0; i < Lbx_files.Items.Count; i++)
+                    {
+                        RemoveTags(Lbx_files.Items[i].ToString());
+                    }
+                    break;
+                case DialogResult.Cancel:
+                    return;
+            }
+        }
+
+        private void Btn_erase_Click(object sender, EventArgs e)
+        {
+            RemoveTags(Txt_selectedSong.Text);
+        }
+
+        void RemoveTags(string file)
+        {
+            try
+            {
+                TagLib.File MetaFile = TagLib.File.Create(Txt_folderSelected.Text + "\\" + file);
+                MetaFile.RemoveTags(TagTypes.AllTags);
+                Rtb_output.AppendText("\n" + file + " : Tags have been removed");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "An Error has Occured", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
         }
     }
 }
